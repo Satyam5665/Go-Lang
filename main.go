210101a648649374e4480cdc8f4a1e3c2544bba4
Satyam Singh <96643434+Satyam5665@users.noreply.github.com>
2024-05-24 21:31:44 +0530

    Variables in Go

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
-	myutil "learningFun/myUtil"
 )
 
 func main() {
 	fmt.Println("Hi")
 
-	myutil.PrintMessage("First Message")
+	var name string = "Satyam"
+	var version = 1.2
+	version = 4
+	const age = 4
+	person := "Satyam Singh"
+	fmt.Println((person))
+	fmt.Println((version))
+	fmt.Println(name)
+	fmt.Println((age))
+
+	var Public = "data is important"
+	var privatevariable = "data will be used only here"
+	fmt.Println((privatevariable))
+	fmt.Println((Public))
 }
 
-
+//If a variable is initialized in Capital Letter then it can be imported in other foles
 
 //Pointers:
 //Packages are tightly coupled to a Directory.
