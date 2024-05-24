385b4e215b3a4dcebd71210fc8f08345699ff92a
Satyam Singh <96643434+Satyam5665@users.noreply.github.com>
2024-05-24 21:49:22 +0530

    Println vs Prinrf

---
+++ print/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 21
+	// name := "Satyam"
+	var name string = "Satyam"
+	fmt.Println("age ", age, "name ", name)
+	fmt.Println("Hi!")
+
+	fmt.Printf("name is %s", name)
+}
