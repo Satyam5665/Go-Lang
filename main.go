2ac7a5e2c84a321d33115abcd89b355865976355
Satyam Singh <96643434+Satyam5665@users.noreply.github.com>
2024-05-24 20:41:50 +0530

    Created Utils Folder

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
-import "fmt"
 
-func main(){
+import (
+	"fmt"
+	myutil "learningFun/myUtil"
+)
+
+func main() {
 	fmt.Println("Hi")
+
+	myutil.PrintMessage("First Message")
 }
+
+
+
+//Pointers:
+//Packages are tightly coupled to a Directory.
+//In one directory, only one package can be present, if you want to create more than one, than simply create anothe
+// Folder.
