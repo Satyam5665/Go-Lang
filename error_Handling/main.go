5668e4b5ae9bfffd117909c27935ad770bb0a821
Satyam Singh <96643434+Satyam5665@users.noreply.github.com>
2024-05-27 01:01:53 +0530

    Error Handling in Go

--- error_Handling/main.go
+++ error_Handling/main.go
@@ -2,11 +2,16 @@ package main
 
 import "fmt"
 
-func divide(a, b float32) float32 {
-	return a / b
+func divide(a, b float32) (float32, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("denominator can't be zero")
+	}
+	return a / b, nil
 }
+
+//We can also use _ , it is a read only variable type
 func main() {
 	fmt.Println("Error Handling in the Function")
-	ans := divide(10, 4)
+	ans, _ := divide(10, 0)
 	fmt.Println("Division of two numbers : ", ans)
 }
