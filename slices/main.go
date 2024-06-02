1df5bd1650cf691a70ff588e0d32189643dc305e
Satyam Singh <96643434+Satyam5665@users.noreply.github.com>
2024-06-02 18:49:03 +0530

    how to create empty slice

--- slices/main.go
+++ slices/main.go
@@ -8,12 +8,17 @@ import "fmt"
 //In slice --> cap(capacity) and size
 //Capacity doubles by 2 if there is requirement
 
+//use make function to create empty slice
+
 func main() {
 	fmt.Println("Hello")
 	number := []int{1, 23, 4, 4}
 	stringsData := []string{"hello, hi, bye"}
 	fmt.Println(number)
 	fmt.Println(stringsData)
+
+	stock := make([]int, 0)
+	fmt.Println(stock)
 	number = append(number, 2, 20, 30)
 	fmt.Printf("number has a data type : %T\n", number)
 
